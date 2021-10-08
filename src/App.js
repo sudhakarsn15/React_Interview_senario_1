0e9b795789d4ff3ec7d1de67afbe39ab4a49daaa
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-10-08 23:35:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Reactjs senario 1</h1>
+      <button>Fetch data</button>
+    </div>
+  );
+}
