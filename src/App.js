5bf2d78092f4c37c7d538a49b710abb8cb0b9181
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-10-09 00:47:46 +0530

    created files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
   render() {
     return (
       <div>
-        <h2>Interview senario 1</h2>
-        <p>1. Write a simple React class which fetches data from given api</p>
+        v<h2>Interview senario 1</h2>
+        <p>
+          1. Write a simple React class which fetches data from given api when
+          page loads
+        </p>
         <p>2. Storing fetched api to redux.</p>
         <p>3. Connet react and redux.</p>
         <p>4. Map fetch api and display in table format</p>
@@ -13,3 +22,5 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
   rootElement
 );
