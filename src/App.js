bf25103eeb20dc5d1c36d9f2f4cd9b8d1c1e711f
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-10-08 23:48:27 +0530

    added senario added senario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./styles.css";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Reactjs senario 1</h1>
-      <button>Fetch data</button>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Interview senario 1</h2>
+        <p>1. Write a simple React class which fetches data from given api</p>
+        <p>2. Storing fetched api to redux.</p>
+        <p>3. Connet react and redux.</p>
+        <p>4. Map fetch api and display in table format</p>
+      </div>
+    );
+  }
 }
